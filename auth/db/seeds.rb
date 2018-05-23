26cb12388455c69d7093c95358d2f6bf5606e1f2
Juan Pablo Gaviria <jgavirias13@gmail.com>
2018-05-23 02:41:07 -0500

    Sincronización del trabajo

--- auth/db/seeds.rb
+++
@@ -1,6 +0,0 @@
-# Se agrega un nuevo usuario para usar como prueba en la autenticacion
-user = User.new
-user.email = 'jgaviri6@eafit.edu.co'
-user.password = '123456'
-user.password_confirmation = '123456'
-user.save!
